657b873982dd815c414283e43cf1cf4817d33aa9
J.Manuel Montoya <folken718@gmail.com>
2020-10-23 01:31:46 -0500

    refactored ISender interface

--- src/Interfaces/ISender.ts
+++ src/Interfaces/ISender.ts
@@ -1,3 +1,7 @@
+import amqp from 'amqplib';
+
 export interface ISender {
+  createConnection(): Promise<amqp.Connection>;
+  init(): Promise<amqp.Channel>;
   sendMessage(queue: string, msg: ArrayBuffer): Promise<void>;
 }
